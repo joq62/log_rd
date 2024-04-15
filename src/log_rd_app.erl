910dfb91ab52cfe8bdcb6ad0efbb159a90f14a64
joq62 <joq62@c50>
2024-04-16 01:34:19 +0200

    put init i app

--- src/log_rd_app.erl
+++ src/log_rd_app.erl
@@ -4,13 +4,21 @@
 %%%-------------------------------------------------------------------
 
 -module(log_rd_app).
+-include("log_rd.hrl").
 
 -behaviour(application).
 
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    log_rd_sup:start_link().
+    {ok,Pid}=log_rd_sup:start_link(),
+    
+    file:del_dir_r(?MainLogDir),
+    ok=file:make_dir(?MainLogDir),
+    [NodeName,_HostName]=string:tokens(atom_to_list(node()),"@"),
+    NodeNodeLogDir=filename:join(?MainLogDir,NodeName),
+    ok=log:create_logger(NodeNodeLogDir,?LocalLogDir,?LogFile,?MaxNumFiles,?MaxNumBytes),
+    {ok,Pid}.
 
 stop(_State) ->
     ok.
--- test/all.erl
+++ test/all.erl
@@ -64,12 +64,7 @@ setup()->
     io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME}]),
     ok=application:start(log_rd),
     
-    file:del_dir_r(?MainLogDir),
-    ok=file:make_dir(?MainLogDir),
-    [NodeName,_HostName]=string:tokens(atom_to_list(node()),"@"),
-    NodeNodeLogDir=filename:join(?MainLogDir,NodeName),
-    ok=log:create_logger(NodeNodeLogDir,?LocalLogDir,?LogFile,?MaxNumFiles,?MaxNumBytes),
-  
+    
     pong=log:ping(),
     pong=rd:ping(),
     
